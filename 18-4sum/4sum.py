a54a502b860edccf73e5b949d7f9564d5afe110d
Prashant jadhav <prashantsureshjadhav409@gmail.com>
2026-08-02 14:32:13 +0530

    Time: 503 ms (86.74%) | Memory: 12.3 MB (93.94%) - LeetSync

--- 18-4sum/4sum.py
+++ 18-4sum/4sum.py
@@ -1,19 +1,46 @@
 class Solution(object):
     def fourSum(self, nums, target):
+        nums.sort()
         n = len(nums)
-        ans = set()
+        ans = []
 
-        for i in range(n):
-            for j in range(i + 1, n):
-                seen = set()
+        for i in range(n - 3):
 
-                for k in range(j + 1, n):
-                    need = target - (nums[i] + nums[j] + nums[k])
+            # Skip duplicate i
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
 
-                    if need in seen:
-                        quad = tuple(sorted([nums[i], nums[j], nums[k], need]))
-                        ans.add(quad)
+            for j in range(i + 1, n - 2):
 
-                    seen.add(nums[k])
+                # Skip duplicate j
+                if j > i + 1 and nums[j] == nums[j - 1]:
+                    continue
 
-        return [list(x) for x in ans]
+                k = j + 1
+                l = n - 1
+
+                while k < l:
+                    s = nums[i] + nums[j] + nums[k] + nums[l]
+
+                    if s < target:
+                        k += 1
+
+                    elif s > target:
+                        l -= 1
+
+                    else:
+                        ans.append([nums[i], nums[j], nums[k], nums[l]])
+
+                        k += 1
+                        l -= 1
+
+                        # Skip duplicate k
+                        while k < l and nums[k] == nums[k - 1]:
+                            k += 1
+
+                        # Skip duplicate l
+                        while k < l and nums[l] == nums[l + 1]:
+                            l -= 1
+
+        return ans
+        
