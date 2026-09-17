a369a0b7514c877e40ade2fca64e3dc720ab5b9a
Prashant jadhav <prashantsureshjadhav409@gmail.com>
2026-09-17 12:58:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Count Reverse Pairs/count-reverse-pairs.py
@@ -0,0 +1,53 @@
+class Solution:
+    def countRevPairs(self, arr):
+        self.count = 0
+
+        def merge(low, mid, high):
+            temp = []
+
+            left = low
+            right = mid + 1
+
+            j = mid + 1
+
+            for i in range(low, mid + 1):
+                while j <= high and arr[i] > 2 * arr[j]:
+                    j += 1
+
+                self.count += j - (mid + 1)
+
+            while left <= mid and right <= high:
+
+                if arr[left] <= arr[right]:
+                    temp.append(arr[left])
+                    left += 1
+
+                else:
+                    temp.append(arr[right])
+                    right += 1
+
+            while left <= mid:
+                temp.append(arr[left])
+                left += 1
+
+            while right <= high:
+                temp.append(arr[right])
+                right += 1
+
+            for i in range(len(temp)):
+                arr[low + i] = temp[i]
+
+        def mergeSort(low, high):
+            if low >= high:
+                return
+
+            mid = (low + high) // 2
+
+            mergeSort(low, mid)
+            mergeSort(mid + 1, high)
+
+            merge(low, mid, high)
+
+        mergeSort(0, len(arr) - 1)
+
+        return self.count
